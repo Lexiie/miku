5f097594ce9205958800e3f125612674dfb82a65
Lexiie <lexiie.casanova@gmail.com>
2026-04-08 06:12:52 +0000

    Stabilize Android IME and project entry

--- android/app/src/main/java/com/miku/MainActivity.kt
+++ android/app/src/main/java/com/miku/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -547,8 +546,7 @@ private fun ComposerBar(
         shape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp),
         modifier = modifier
             .fillMaxWidth()
-            .navigationBarsPadding()
-            .imePadding(),
+            .navigationBarsPadding(),
     ) {
         Row(
             modifier = Modifier
